7c61c0a0fce15e6319626e33c6cd4835495d2a07
chern <chernenkij,nikitacze@gmail.com>
2023-07-11 17:14:03 +0200

    2 Курс ДЗ №18 Работа с исключениями

--- src/UserValidator.java
+++ src/UserValidator.java
@@ -1,12 +1,9 @@
 public class UserValidator {
     private static final String REGEX = "^[a-zA-Z0-9_]*$";
-
     public static void validateUser(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
 
         validateLogin(login);
-
         validatePassword(password);
-
         validatePasswordMatch(password, confirmPassword);
 
     }
@@ -14,7 +11,6 @@ public class UserValidator {
     private static void validateLogin(String login) throws WrongLoginException {
 
         if (login.length() > 20 || !login.matches("REGEX")) {
-
             throw new WrongLoginException("Неверный логин");
 
         }
@@ -22,9 +18,7 @@ public class UserValidator {
     }
 
     private static void validatePassword(String password) throws WrongPasswordException {
-
         if (password.length() > 20 || !password.matches("REGEX")) {
-
             throw new WrongPasswordException("Неверный пароль");
 
         }
@@ -32,9 +26,7 @@ public class UserValidator {
     }
 
     private static void validatePasswordMatch(String password, String confirmPassword) throws WrongPasswordException {
-
         if (!password.equals(confirmPassword)) {
-
             throw new WrongPasswordException("Пароли не совпадают");
 
         }
