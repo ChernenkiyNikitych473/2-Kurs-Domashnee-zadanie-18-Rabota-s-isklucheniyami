801e4295e69590756be6f6dfba28ecbe607badc0
chern <chernenkij,nikitacze@gmail.com>
2023-07-11 17:46:27 +0200

    2 Курс ДЗ №18 Работа с исключениями

--- src/UserValidator.java
+++ src/UserValidator.java
@@ -7,29 +7,23 @@ public class UserValidator {
         validatePasswordMatch(password, confirmPassword);
 
     }
-
     private static void validateLogin(String login) throws WrongLoginException {
 
         if (login.length() > 20 || !login.matches("REGEX")) {
             throw new WrongLoginException("Неверный логин");
 
         }
-
     }
-
     private static void validatePassword(String password) throws WrongPasswordException {
         if (password.length() > 20 || !password.matches("REGEX")) {
             throw new WrongPasswordException("Неверный пароль");
 
         }
-
     }
-
     private static void validatePasswordMatch(String password, String confirmPassword) throws WrongPasswordException {
         if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException("Пароли не совпадают");
 
         }
-
     }
 }
